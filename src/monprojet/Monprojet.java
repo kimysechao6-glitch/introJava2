58faefd324b1412df6d1a6187e573ddc0486b481
kimy <kimysechao@gmail.com>
2026-03-24 14:36:05 +0100

    exo 2

--- src/monprojet/Monprojet.java
+++ src/monprojet/Monprojet.java
@@ -14,15 +14,11 @@ public class Monprojet {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        char lettre = 'k';
-        int nbr = '6';
-        long nbrl = 23456789;
-        float nbvrgl = 12.7f;
-        double gnbr = 54.67899;
-        boolean vlrft = true;
-        String chcarac = "ce matin il fait beau";
-        
-        
+        int age = 2026 - 2002;
+        System.out.println(age);
+        String nomComplet ="Pierre" +" " + "Dupont";
+        System.out.println(nomComplet + " est ne en 2002 donc il a "+ age + " ans");
+
         
     }
     
