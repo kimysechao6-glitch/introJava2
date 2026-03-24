99a73d111ad4e8332ee530093ff033d743095387
kimy <kimysechao@gmail.com>
2026-03-24 14:25:18 +0100

    first commit et ex 1

---
+++ src/monprojet/Monprojet.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package monprojet;
+
+/**
+ *
+ * @author Kimy
+ */
+public class Monprojet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        char lettre = 'k';
+        int nbr = '6';
+        long nbrl = 23456789;
+        float nbvrgl = 12.7f;
+        double gnbr = 54.67899;
+        boolean vlrft = true;
+        String chcarac = "ce matin il fait beau";
+        
+        
+        
+    }
+    
+}
